bd7719e569f319802c8ac7070fb7f4206bdba01f
JordiGG <jordi.grados@codurance.com>
2024-04-23 08:58:30 +0200

    WIP tic tac toe

--- Katas/TicTacToe/App/Cli/CliBoardDrawer.cs
+++ Katas/TicTacToe/App/Cli/CliBoardDrawer.cs
@@ -6,6 +6,11 @@ public class CliBoardDrawer : IBoardDrawer
 {
     public string Draw(char[] boardState)
     {
+        if (boardState is not { Length: 9 })
+        {
+            throw new ArgumentException("Board state must exactly contain 9 elements.");
+        }
+        
         var builder = new StringBuilder();
         builder.AppendLine("+---+---+---+");
         for (var i = 0; i < 9; i += 3)
--- Katas/TicTacToe/Tests/Cli/CliBoardDrawerShould.cs
+++ Katas/TicTacToe/Tests/Cli/CliBoardDrawerShould.cs
@@ -5,6 +5,14 @@ namespace Katas.TicTacToe.Tests.Cli;
 
 public class CliBoardDrawerShould
 {
+    private CliBoardDrawer _drawer;
+
+    [SetUp]
+    public void Setup()
+    {
+        _drawer = new CliBoardDrawer();
+    }
+
     [TestCase(
         new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' },
         "+---+---+---+\n" +
@@ -15,13 +23,13 @@ public class CliBoardDrawerShould
         "| 7 | 8 | 9 |\n" +
         "+---+---+---+\n")]
     [TestCase(
-        new char[] { 'X', 'O', '3', '4', '5', '6', '7', '8', '9' },
+        new char[] { 'X', 'O', '3', '4', '5', '6', '7', '8', 'X' },
         "+---+---+---+\n" +
         "| X | O | 3 |\n" +
         "+---+---+---+\n" +
         "| 4 | 5 | 6 |\n" +
         "+---+---+---+\n" +
-        "| 7 | 8 | 9 |\n" +
+        "| 7 | 8 | X |\n" +
         "+---+---+---+\n")]
     [TestCase(
         new char[] { 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', '9' },
@@ -34,10 +42,29 @@ public class CliBoardDrawerShould
         "+---+---+---+\n")]
     public void DrawBoard_CorrectlyBasedOnInput(char[] boardState, string expected)
     {
-        var game = new CliBoardDrawer();
-
-        var drawnBoard = game.Draw(boardState);
+        var drawnBoard = _drawer.Draw(boardState);
 
         Assert.That(drawnBoard, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ThrowArgumentException_WhenBoardStateIsNull()
+    {
+        Assert.That(
+            () => _drawer.Draw(null),
+            Throws.TypeOf<ArgumentException>()
+                .With.Message.EqualTo("Board state must exactly contain 9 elements.")
+        );
+    }
+
+    [Test]
+    public void ThrowArgumentException_WhenBoardStateIsNotExactly9Elements()
+    {
+        var boardState = new char[] { 'X' };
+        Assert.That(
+            () => _drawer.Draw(boardState),
+            Throws.TypeOf<ArgumentException>()
+                .With.Message.EqualTo("Board state must exactly contain 9 elements.")
+        );
     }
 }
