eb3ace3968f620ae531e5f4efe5997902e03158b
JordiGG <jordi.grados@codurance.com>
2024-05-08 10:35:05 +0200

    Gilded rose

---
+++ Katas/GildedRose/Tests/GildedRoseShould.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace Katas.GildedRose.Tests;
+
+public class GildedRoseShould
+{
+    [Test]
+    public void BeTrue()
+    {
+        Assert.That(true, Is.True);
+    }
+}
